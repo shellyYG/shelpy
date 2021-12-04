42894c156bcdc5172021c1b7cea2e760d975d25f
shellyyg <chih-ching.yang@whu.edu>
2021-12-04 20:45:40 +0800

    💄 Change general Background color from black to #231f20

--- client/src/components/NavBar/icon.js
+++ client/src/components/NavBar/icon.js
@@ -2,7 +2,7 @@ import { IconContext } from 'react-icons';
 import { CgGlobeAlt } from 'react-icons/cg';
 const GlobalIcon = () => {
   return (
-    <IconContext.Provider value={{ color: 'black', size: '40' }}>
+    <IconContext.Provider value={{ color: 'white', size: '40' }}>
       <div>
         <CgGlobeAlt />
       </div>
