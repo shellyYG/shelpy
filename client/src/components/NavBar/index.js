7a187c7b3661a84383178d76b96187ab6257d61b
shellyyg <chih-ching.yang@whu.edu>
2021-12-31 15:43:48 +0800

    🐛 Use react router v6 standard to fix active nav link not shown as active issue

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -3,23 +3,65 @@ import { NavLink } from 'react-router-dom';
 import GlobalIcon from './icon';
 
 const NavBar = () => {
+  const nonActiveStyle = {
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+    padding: "10px 22px",
+    cursor: "pointer",
+    border: "1px solid #fff995",
+    borderRadius: "4px",
+    margin: "3px",
+  };
+  const activeStyle = {
+    color: 'black',
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    padding: '10px 22px',
+    cursor: 'pointer',
+    border: '1px solid #fff995',
+    backgroundColor: '#fff995',
+    borderRadius: '4px',
+    margin: '3px',
+    fontWeight: 'bold',
+  }
   return (
     <>
       <nav>
         <div className="nav-menu">
-          <NavLink className="nav-logo" to="/home" exact={true} activeStyle>
-            <img src={'/Shelpy_logo.jpg'} alt={'logo'} />
+          <NavLink
+            exact
+            className="nav-logo"
+            to="/home"
+            exact={true}
+            activeStyle
+          >
+            <img src={"/Shelpy_logo.jpg"} alt={"logo"} />
           </NavLink>
-          <NavLink className="nav-link" to="/about" activeStyle>
+          <NavLink
+            exact
+            style={({ isActive }) => (isActive ? activeStyle : nonActiveStyle)}
+            to="/about"
+          >
             About
           </NavLink>
-          <NavLink className="nav-link" to="/service-options" activeStyle>
+          <NavLink
+            exact
+            style={({ isActive }) => (isActive ? activeStyle : nonActiveStyle)}
+            to="/service-options"
+          >
             Book a Shelper
           </NavLink>
         </div>
         <div className="nav-button">
-          <NavLink className="nav-button-link" to="/sign-up">
-            Sign In
+          <NavLink
+            exact
+            style={({ isActive }) => (isActive ? activeStyle : nonActiveStyle)}
+            to="/sign-in"
+          >
+            Sign-In
           </NavLink>
           <GlobalIcon />
         </div>
