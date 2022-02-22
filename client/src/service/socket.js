46bc05096be844345277dceefdb3649080075a84
shellyyg <chih-ching.yang@whu.edu>
2022-02-22 23:06:55 +0800

    🐛 Try fixing net::ERR_SSL_PROTOCOL_ERROR

--- client/src/service/socket.js
+++ client/src/service/socket.js
@@ -1,7 +1,7 @@
 // Create a separate file from pages/components 
 // to avoid multiple connection in React life-cycle
 
-import io from "socket.io-client";
-export const socket = io.connect('https://shelpy.co:9000'); // Develop: http://localhost:9000
+import { io } from "socket.io-client";
+export const socket = io(); // Develop: export const socket = io.connect('https://shelpy.co:9000');
 // Standard way:
 // export const socket = io(SOCKET_URL);
