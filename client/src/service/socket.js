1b0d3d51ab84d19954ddfe3c70672d119d6c7510
shellyyg <chih-ching.yang@whu.edu>
2022-03-14 20:39:37 +0800

    🔖 Version 1.2.0

--- client/src/service/socket.js
+++ client/src/service/socket.js
@@ -2,7 +2,7 @@
 // to avoid multiple connection in React life-cycle
 import { io } from 'socket.io-client';
 
-const isDeveloping = 1;
+const isDeveloping = 0; // TODO before push to ec2
 const connectUrl = isDeveloping ? 'http://localhost:9000' : 'https://shelpy.co';
 
 export const socket = io.connect(connectUrl);
