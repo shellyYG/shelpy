c8126d3cfa1c4dfbb06f4981045de8b825192c61
shellyyg <chih-ching.yang@whu.edu>
2022-03-21 16:06:26 +0800

    ✨ Add Agreement for Work english translation

--- client/src/pages/HelperContractPage.js
+++ client/src/pages/HelperContractPage.js
@@ -28,77 +28,68 @@ const HelperContractPage = () => {
           }}
         >
           <div>
-            <h1>過來人 {t('helper_terms')}</h1>
+            <h1>
+              {t('shelpyltd')} {t('helper_terms')}
+            </h1>
           </div>
 
           <div style={{ width: '80%', margin: 'auto', textAlign: 'start' }}>
             <div style={{ margin: '10px auto' }}>
-              過來人有限公司(以下簡稱「本公司」或「Shelpy」)與承攬諮詢廠商(以下簡稱「過來人」)
-              訂有「勞務承攬合約」，故訂定本服務條款供過來人於提供服務時以玆遵循，過來人於每次執行視訊諮詢服務時，均同意本服務條款之約定：
-              <br />
-              <br />
-              <p style={{ fontSize: '16px', fontWeight: 'bold' }}>前言</p>
-              1、本公司為保障消費者權益、廣告暨恪遵政府法令及相關規範，爰訂定本服務條款。
-              <br />
-              2、基於前言第一款意旨，本公司對本服務條款規定保有最終解釋之權利。
-              <br />
-              3、本公司對違反服務條款規定之事實認定保有最終認定權利。
+              {t('helper_terms_intro1')}
+              {t('helper_terms_intro2')}
               <br />
               <br />
               <p style={{ fontSize: '16px', fontWeight: 'bold' }}>
-                一般條款
-              </p>{' '}
-              1、 過來人 應提供的服務，包括但不限於：
+                {t('intro_title')}
+              </p>
+              {t('t1_why_helper_terms')}
               <br />
-              與消費者在指定日期、指定時間、在網路上履行諮詢服務。
+              {t('t2_helper_terms_explanation_power')}
               <br />
-              2、為保障消費者權益與服務品質，過來人在提供服務時，應避免應避免下述行為，或有發生疑似類似的情形，
-              <br />
-              本公司可能暫時停止派單並考慮後續是否繼續合作關係：
-              <br />
-              (1) 未準時出席會議。 <br />
-              (2) 口語之衝突、侮辱。 <br />
-              (3) 欺騙、偷竊、妨害名譽、傷害或破壞之行為。
-              <br />
-              (4) 不正常或不道德之交易。 <br />
-              (5) 經由任何形式或管道洩漏第三人之個人資料
-              （包括但不限於姓名、出生年月日、身分證字
-              號、統一編號、聯絡方式（含電話、地址）），或利用取得之個人資料侵犯第三人之隱私及
-              自由(包括但不限於騷擾第三人)。
-              <br />
-              (6) 洩漏與承攬服務無關之未公開資訊。
-              <br />
-              (7)
-              利用本公司的資源拿到消費者資料後、與消費者私下交易、而未透過本公司平台。
+              {t('t3_helper_terms_violation')}
               <br />
               <br />
               <p style={{ fontSize: '16px', fontWeight: 'bold' }}>
-                承攬報酬結算
+                {t('general_conditions_terms')}
               </p>{' '}
-              1、 報酬內容：依本公司公告現時費率為準，目前為交易金額之85%
-              (註：最後收到的實際款項會略少於
-              85%，是因為使用PayPal會有轉帳手續費的關係，由於 PayPal
-              手續費多寡是視過來人帳號在哪個國家而定，無法事先預知，於是手續費目前由過來人自行吸收)。
+              {t('t1_helper_service_includes_d1')}
               <br />
-              2、 計算方式：依本公司公告費率為主。
+              {t('t1_helper_service_includes_d2')}
               <br />
-              3、 報酬發放日：每月 28 日，遇假日本公司可能提前或延後發放。
+              {t('t2_helper_service_protect_helpee_d1')}
               <br />
-              4、 報酬領取方式：每月 28 日，經由 PayPal 轉帳至過來人的 PayPal
-              帳戶。
+              {t('t2_helper_service_protect_helpee_d2')}
+              <br />
+              {t('t2_helper_service_protect_helpee_t1')} <br />
+              {t('t2_helper_service_protect_helpee_t2')} <br />
+              {t('t2_helper_service_protect_helpee_t3')} <br />
+              {t('t2_helper_service_protect_helpee_t4')} <br />
+              {t('t2_helper_service_protect_helpee_t5')} <br />
+              {t('t2_helper_service_protect_helpee_t6')} <br />
+              {t('t2_helper_service_protect_helpee_t7')} <br />
+              <br />
+              <p style={{ fontSize: '16px', fontWeight: 'bold' }}>
+                {t('helper_terms_payment_terms')}
+              </p>{' '}
+              {t('t1_helper_terms_payment_detail1')}
+              {t('t1_helper_terms_payment_detail2')}
+              <br />
+              {t('t2_helper_terms_calculation')}
+              <br />
+              {t('t3_helper_terms_calculation')}
+              <br />
+              {t('t4_helper_terms_payment_ways')}
               <br />
               <br />
               <p style={{ fontSize: '16px', fontWeight: 'bold' }}>
-                暫停派單/終止外送合約
+                {t('helper_terms_termination')}
               </p>{' '}
-              如本公司合理懷疑以下事項，可能未經通知立即或在後續承攬期間暫時停止派單，並依情節輕重
-              考慮後續合作關係： <br />
-              (1) 違反承攬服務條款。 <br />
-              (2) 發生重大事件，涉及危害公司商譽。 <br />
-              (3) 違反相關法令或政府規範。 <br />
-              (4) 私下接單。 <br />
-              (5)
-              違反誠實信用(如詐欺、侵占、不正常交易等)、善良風俗、公共秩序之行為。
+              {t('helper_terms_termination_intro')} <br />
+              {t('helper_terms_termination_t1')} <br />
+              {t('helper_terms_termination_t2')} <br />
+              {t('helper_terms_termination_t3')} <br />
+              {t('helper_terms_termination_t4')} <br />
+              {t('helper_terms_termination_t5')} <br />
             </div>
           </div>
         </div>
